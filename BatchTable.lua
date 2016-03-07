3b7e0d795e06868f3d0206a00508fff7f2ac09f8
ppaanngggg <ppaanngggg@gmail.com>
2016-03-07 16:31:04 +0800

    recode Cycle.lua

--- BatchTable.lua
+++ BatchTable.lua
@@ -15,7 +15,6 @@ end
 function BatchTable:updateOutput(input)
     assert(torch.type(input) == 'table', "input should be table")
     self.output = {}
-    param, gradParam = self.module:parameters()
     for i = 1,#input do
         self.sharedModules[i] = self.sharedModules[i] or self.module:clone('weight', 'bias', 'gradWeight', 'gradBias')
         self.output[i] = self.sharedModules[i]:forward(input[i])
