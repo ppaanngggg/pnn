5c078a04be269de8a3013e7ae0850d9cf75aeff5
ppaanngggg <ppaanngggg@gmail.com>
2016-03-16 14:07:04 +0800

    fix cycle rho==1 bug

--- Cycle.lua
+++ Cycle.lua
@@ -44,31 +44,39 @@ function Cycle:updateGradInput(input, gradOutput)
     assert(#gradOutput == self.rho, "#gradOutput must be equal with rho")
 
     self._combinedGradInput = {}
-    for i = self.rho,1,-1 do
-        if i == self.rho then
-            self._combinedGradInput[self.rho] =
-                self.sharedModules[self.rho]:updateGradInput(
-                    self.output[self.rho - 1],
-                    gradOutput[self.rho]
-                )
-        else
-            self._combinedGradInput[i + 1]:add(gradOutput[i])
-            if i == 1 then
-                self._combinedGradInput[i] =
-                    self.sharedModules[i]:updateGradInput(
-                        realInput,
-                        self._combinedGradInput[i + 1]
+    if self.rho == 1 then
+        self._combinedGradInput[1] =
+            self.sharedModules[1]:updateGradInput(
+                realInput,
+                gradOutput[1]
+            )
+    else
+        for i = self.rho,1,-1 do
+            if i == self.rho then
+                self._combinedGradInput[self.rho] =
+                    self.sharedModules[self.rho]:updateGradInput(
+                        self.output[self.rho - 1],
+                        gradOutput[self.rho]
                     )
             else
-                self._combinedGradInput[i] =
-                    self.sharedModules[i]:updateGradInput(
-                        self.output[i - 1],
-                        self._combinedGradInput[i + 1]
-                    )
+                self._combinedGradInput[i + 1]:add(gradOutput[i])
+                if i == 1 then
+                    self._combinedGradInput[i] =
+                        self.sharedModules[i]:updateGradInput(
+                            realInput,
+                            self._combinedGradInput[i + 1]
+                        )
+                else
+                    self._combinedGradInput[i] =
+                        self.sharedModules[i]:updateGradInput(
+                            self.output[i - 1],
+                            self._combinedGradInput[i + 1]
+                        )
+                end
             end
         end
     end
-    self.gradInput = {self._combinedGradInput[1], torch.Tensor(1):typeAs(input[2])}
+    self.gradInput = {self._combinedGradInput[1], torch.zeros(1):typeAs(input[2])}
     return self.gradInput
 end
 
@@ -77,19 +85,25 @@ function Cycle:accGradParameters(input, gradOutput, scale)
     assert(torch.type(input) == 'table', "expecting input table")
     local realInput = input[1]
     assert(#gradOutput == self.rho, "#gradOutput must be equal with rho")
-    for i = self.rho,1,-1 do
-        if i == self.rho then
-            self.sharedModules[self.rho]:accGradParameters(
-                self.output[self.rho - 1], gradOutput[self.rho], scale
-            )
-        elseif i == 1 then
-            self.sharedModules[i]:accGradParameters(
-                realInput, self._combinedGradInput[i + 1], scale
-            )
-        else
-            self.sharedModules[i]:accGradParameters(
-                self.output[i - 1], self._combinedGradInput[i + 1], scale
-            )
+    if self.rho == 1 then
+        self.sharedModules[1]:accGradParameters(
+            realInput, gradOutput[1], scale
+        )
+    else
+        for i = self.rho,1,-1 do
+            if i == self.rho then
+                self.sharedModules[self.rho]:accGradParameters(
+                    self.output[self.rho - 1], gradOutput[self.rho], scale
+                )
+            elseif i == 1 then
+                self.sharedModules[i]:accGradParameters(
+                    realInput, self._combinedGradInput[i + 1], scale
+                )
+            else
+                self.sharedModules[i]:accGradParameters(
+                    self.output[i - 1], self._combinedGradInput[i + 1], scale
+                )
+            end
         end
     end
 end
